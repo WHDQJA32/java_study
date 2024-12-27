52065e79b410b5ba55ae10c562193e491ba92847
whdqja32 <whdqja32@naver.com>
2024-12-27 09:19:26 +0900

    추상클래스

---
+++ src/study/abst/Coke.java
@@ -0,0 +1,13 @@
+package study.abst;
+
+//콜라
+public class Coke extends Beverage {
+	int sugarGram; //당분 용량
+
+	@Override
+	public void checkSafety() {
+		System.out.println("성분 정상인가");
+		// TODO Auto-generated method stub 재정의해야한다.
+		
+	}
+}
---
+++ src/study/abst/Tera.java
@@ -0,0 +1,13 @@
+package study.abst;
+
+//테라 맥주
+public class Tera extends Beverage {
+	double alcol; //알콜도수 5.5%
+
+	@Override
+	public void checkSafety() {
+		System.out.println("도수가 맞냐?");
+		// TODO Auto-generated method stub 재정의해야한다.
+		
+	}
+}
