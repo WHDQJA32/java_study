8528777ededd07dbdb6b7e65d7863f1395dd81e4
whdqja32 <whdqja32@naver.com>
2024-12-27 17:14:38 +0900

    스레드 처리

---
+++ src/study/thread/case2/Work.java
@@ -0,0 +1,37 @@
+package study.thread.case2;
+
+public class Work implements Runnable {
+	
+	int workCount;
+	int workLoad;
+	
+	public Work(int workLoad) {
+		this.workLoad = workLoad;
+		workCount = 0;
+	}
+	
+	public void addCount() {
+		workCount++;
+	}
+	
+	@Override
+	public void run() {
+		
+		while(workCount < workLoad) {
+		
+			synchronized (this) {
+				addCount();
+				System.out.println(Thread.currentThread().getName()+ " workCount : "+ workCount);
+			}
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
